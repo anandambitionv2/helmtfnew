32cb55d971096656bb632d23a11b882f33f456cd
anandazaugust <anandazaugust@outlook.com>
2026-09-22 06:12:26 +0530

    tfapply

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "releases" {
+  description = "A map of Helm releases to be deployed"
+  type        = map(object({
+    name             = string
+    repository       = string
+    chart            = string
+    version          = string
+    values           = optional(list(string), [])
+    namespace        = string
+    create_namespace = bool
+  }))
+}
