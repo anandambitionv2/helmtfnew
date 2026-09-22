0d82ac46422b14d4d4cd5d74f73d071fcf6c40f4
anandazaugust <anandazaugust@outlook.com>
2026-09-22 06:57:43 +0530

    tfapply

--- import.tf
+++ import.tf
@@ -1,5 +1,5 @@
 import {
  for_each = {for k,v in var.releases : k=>v if v.import ==true }
- to = module.helm.helm_release[k].id
+ to = module.helm.helm_release[k]
  id = each.value.namespace/each.value.name
 }
