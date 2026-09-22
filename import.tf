da8bb033c7b7fe2fdbdca9536b8fdbac96f7a226
anandazaugust <anandazaugust@outlook.com>
2026-09-23 04:46:49 +0530

    update

--- import.tf
+++ import.tf
@@ -1,5 +1,17 @@
+# import {
+# #  for_each = var.releases
+
+#  to = module.helm.helm_release.example[each.key]
+#  id = "${each.value.namespace}/${each.value.name}"
+# }
+
 import {
- for_each = var.releases
- to = module.helm.helm_release.example[each.key]
- id = "${each.value.namespace}/${each.value.name}"
+  for_each = {
+    for k, v in var.releases : k => v
+    if v.import
+  }
+
+  to = module.helm.helm_release.example[each.key]
+
+  id = "${each.value.namespace}/${each.value.name}"
 }
